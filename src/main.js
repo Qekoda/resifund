524f1bab9813b879f6adcfc5cd73628f54576d31
Qekoda <power22810@mail.ru>
2025-02-07 10:42:20 +0700

    added notification plugin

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from './plugins/axios'
+import Toast from 'vue-toastification'
 
 import './assets/stylesheets/_application.sass'
+import 'vue-toastification/dist/index.css'
+
+Vue.use(Toast)
 
 Vue.config.productionTip = false
 window.axios = axios
