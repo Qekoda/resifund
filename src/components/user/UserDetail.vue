142fc2629f0b35f6e3881cc50134c12666568968
Qekoda <power22810@mail.ru>
2025-02-07 10:46:27 +0700

    finish👍👌

---
+++ src/components/user/UserDetail.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="user-detail">
+    <div class="user-detail__avatar">
+      <img :src="avatarSrc" alt="Avatar" class="avatar" />
+    </div>
+    <div class="user-detail__info">
+      <p class="user-detail__label ">{{ user.name }}</p>
+      <p class="user-detail__label">email: <span class="user-detail__text">{{ user.email }}</span></p>
+      <p class="user-detail__label">phone: <span class="user-detail__text">{{ user.phone }}</span></p>
+      <p class="user-detail__label">О себе: </p>
+      <p class="user-detail__text">{{ user.about || defaultAbout}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        default: () => ({})
+      }
+    },
+    data() {
+      return {
+        defaultAbout: 'Нет информации'
+      }
+    },
+    computed: {
+      avatarSrc() {
+        return this.user.avatar || require('@/assets/images/missing-image.svg')
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+.user-detail 
+  display: flex
+  flex-direction: row
+  gap: 6%
+  &__avatar
+    width: 47%
+    border: 1px solid var(--color-border)
+    text-align: center
+    align-content: center
+    aspect-ratio: 3/2
+
+  &__info
+    display: flex
+    flex-direction: column
+    gap: 10px
+
+  &__text
+    color: var(--color-text)
+    font-size: .875rem
+
+  &__label
+    color: var(--color-dark)
+    font-weight: 600
+
+
+.avatar
+  width: 30%
+
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,22 +1,211 @@
 <template>
-  <div class="bg-gray-100 h-full flex flex-col">
-    <div class="flex justify-between mb-10">
-      <h1 class=" text-red-700 text-3xl font-bold">Жилфонд</h1>
-      <div>Пользователь</div>
-    </div>
-    <div class="flex h-full shadow-lg rounded-lg overflow-hidden">
-      <div class="w-1/3 p-6 bg-gray-700 h-full">
+  <div class="users-block">
+    <div class="users-block__left-part">
+      <div class="block">
+        <p class="block__title">Поиск сотрудников</p>
+        <app-input 
+          placeholder="Введите id или username"
+          v-model.trim="search"
+        />
       </div>
-      <div class=" bg-red-800 h-full" style="width: 4px;"></div>
-      <div class="p-6 bg-gray-400 h-full w-full">
-
+      <div class="block block--hidden">
+        <p class="block__title">Список сотрудников</p>
+        <app-preloader class="users-block__preloader" v-if="loading" />
+        <ul 
+          v-else-if="users.length"
+          class="list list--scroll"
+        >
+          <li 
+            v-for="user in visibleUsers" 
+            :key="user.id"
+          >
+            <user-card 
+              :user="user"
+              :active="selectedUser?.id === user.id"
+              @click.native="selectUser(user)"
+            />
+          </li>
+          <li v-if="visibleCount < users.length">
+            <button 
+              class="button"
+              @click="loadMore" 
+              type="button"
+            >
+              Показать еще
+            </button>
+          </li>
+        </ul>
+        <p 
+          class="list__empty"
+          v-else
+        >Ничего не найдено</p>
+      </div>
+    </div>
+    <div class="users-block__right-part">
+      <user-detail 
+        v-if="selectedUser"
+        :user="selectedUser"
+      />
+      <div
+        class="users-block__empty-profile"
+        v-else
+      >
+        Выберите сотрудника, чтобы посмотреть его профиль
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
+import debounce from 'debounce'
+import { mapState, mapActions } from 'vuex';
 
+const AppInput = () => import('../components/ui/AppInput')
+const UserCard = () => import('../components/user/UserCard')
+const UserDetail = () => import('../components/user/UserDetail')
+const AppPreloader = () => import('../components/ui/AppPreloader')
+
+export default {
+  components: {
+    AppInput,
+    UserCard,
+    UserDetail,
+    AppPreloader
+  },
+  data() {
+    return {
+      search: '',
+      visibleCount: 5,
+      selectedUser: null,
+      loading: false
+    }
+  },
+  computed: {
+    ...mapState('users', [
+      'users'
+    ]),
+    visibleUsers() {
+      return this.users.slice(0, this.visibleCount)
+    },
+  },
+  watch: {
+    search: debounce(function(newValue) {
+      this.getUsersLink(newValue)
+    }, 300)
+  },
+  methods: {
+    ...mapActions('users', [
+      'getUsers',
+      'clearUsers'
+    ]),
+    getUsersLink(value) {
+      if (!value) {
+        this.clearUsers()
+        this.selectedUser = null
+        return
+      }
+      
+      const params = new URLSearchParams()
+      
+      value.split(',')
+        .map(item => item.trim())
+        .filter(item => item)
+        .forEach(item => {
+          if (/^\d+$/.test(item)) { //только цифры
+            params.append('id', item);
+          } else if (/^[a-zA-Z]+$/.test(item)) { // Только буквы
+            params.append('username', item)
+          }
+        })
+        
+      this.loading = true
+      this.getUsers(params)
+        .catch((error) => {
+        this.$toast.error('Произошла ошибка при поиске сотрудников')
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    loadMore() {
+      this.visibleCount += 5
+    },
+    selectUser(user) {
+      this.selectedUser = user
+    } 
+  }
 }
 </script>
+
+<style lang="sass" scoped>
+
+.users-block
+  height: 100%
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.1)
+  border-radius: 10px
+  display: flex
+  flex-direction: row
+  
+  &__left-part
+    min-width: 300px
+    border-right: 1px solid var(--color-border)
+    padding: 30px 20px 
+    display: flex
+    flex-direction: column
+    gap: 30px
+
+  &__right-part
+    padding: 30px 20px
+    width: 100%
+    position: relative
+    
+  &__empty-profile
+    font-size: .875rem
+    color: var(--color-text)
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+
+  &__preloader
+    margin-top: 50px
+.block
+  display: flex
+  flex-direction: column
+  gap: 15px
+  position: relative
+
+  &__title
+    font-weight: bold
+
+  &--hidden
+    overflow: hidden
+
+.list
+  display: flex
+  flex-direction: column
+  gap: 18px
+  padding-right: 20px
+
+  &__empty
+    font-size: .875rem
+    color: var(--color-text)
+
+  &--scroll
+    overflow-y: scroll
+
+.button
+  width: 100%
+  padding: 14px 7px
+  border-radius: 10px
+  border: 1px solid var(--color-border)
+  background-color: mediumspringgreen
+  display: block
+  cursor: pointer
+  
+  &:hover,
+  &:focus,
+  &:active
+    background-color: mediumseagreen
+
+</style>
