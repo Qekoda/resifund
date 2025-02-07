d5c411e96128363c86509ff39ef45222c4a5e3c6
Qekoda <power22810@mail.ru>
2025-02-07 10:45:50 +0700

    added user-card component

---
+++ src/components/user/UserCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="user-card">
+    <div class="user-card__avatar">
+      <img :src="avatarSrc" alt="Avatar" class="avatar" />
+    </div>
+    <div 
+      class="user-card__info"
+      :class="{ 'user-card__info--active': active }"
+    >
+      <p class="user-card__username">{{ user.username }}</p>
+      <p class="user-card__email">{{ user.email }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      user: {
+        type: Object,
+        default: () => ({
+          username: 'noname',
+          email: '',
+          avatar: ''
+        })
+      },
+      active: {
+        type: Boolean,
+        default: false
+      }
+    },
+    computed: {
+      avatarSrc() {
+        return this.user.avatar || require('@/assets/images/missing-image.svg')
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+
+.user-card 
+  height: 70px
+  display: flex
+  flex-direction: row
+  align-items: center
+  width: 100%
+  border: 1px solid var(--color-border)
+  border-radius: 10px
+  overflow: hidden
+  cursor: pointer
+
+  &__info
+    padding: 16px
+    display: flex
+    flex-direction: column
+    gap: 5px
+    font-size: .875rem
+    width: 100%
+
+    &--active
+      background-color: var(--color-border)
+
+  &__username
+    font-weight: 600
+    color: var(--color-dark)
+
+  &__email
+    color: var(--color-text)
+
+  &__avatar
+    padding: 19px
+    border-right: 1px solid var(--color-border)
+    
+.avatar
+  width: 32px
+  height: 32px
+
+</style>
