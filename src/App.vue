b8336c9e5436bffbd5cd93a720e0c58b79886af0
Qekoda <power22810@mail.ru>
2025-02-07 10:43:31 +0700

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <template>
   <div id="app">
-    <div class=" h-screen p-12">
-      <router-view />
+    <div class="container">
+      <app-header />
+      <main class="main">
+        <router-view/>
+      </main>
     </div>
   </div>
 </template>
+
+<script>
+const AppHeader = () => import('./components/AppHeader.vue')
+
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.container
+  height: 100vh
+  padding: 50px
+  display: flex
+  flex-direction: column
+
+.main
+  flex: 1
+  min-height: 300px
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header header--indent-bottom">
+    <h1 class="header__logo">Жилфонд</h1>
+    <div class="header__user">Пользователь</div>
+  </header>
+</template>
+
+<style lang="sass" scoped>
+.header
+  display: flex
+  justify-content: space-between
+  align-items: center
+
+  &__logo
+    font-weight: bold
+    color: var(--color-logo)
+
+  &--indent-bottom
+    margin-bottom: 25px
+</style>
