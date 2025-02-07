2c06dabf9eb1fd3876662ad08c4f75d76894d81e
Qekoda <power22810@mail.ru>
2025-02-07 10:42:54 +0700

    added preloader component

---
+++ src/components/ui/AppPreloader.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="preloader-wrapper">
+    <div class="preloader"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="sass" scoped>
+.preloader-wrapper
+  position: relative
+  height: 60px
+
+.preloader
+  display: block
+  position: absolute
+  top: 50%
+  left: 50%
+  height: 50px
+  width: 50px
+  margin: -25px 0 0 -25px
+  border: 2px solid transparent
+  border-top-color: #000000
+  border-radius: 50%
+  -webkit-animation: spin7 1.5s ease infinite
+          animation: spin7 1.5s ease infinite
+
+  &:before
+    content: ""
+    position: absolute
+    top: 7px
+    right: 7px
+    bottom: 7px
+    left: 7px
+    border: 2px solid transparent
+    border-radius: 50%
+    border-top-color: #000000
+    animation: spin7 3s linear infinite
+
+  &:after
+    content: ""
+    position: absolute
+    top: 15px
+    right: 15px
+    bottom: 15px
+    left: 15px
+    border: 2px solid transparent
+    border-radius: 50%
+    border-top-color: #000000
+    animation: spin7 1.5s ease infinite
+
+@keyframes spin7
+  from
+    transform: rotate(0deg)
+    transform: rotate(0deg)
+  to
+    transform: rotate(359deg)
+    transform: rotate(359deg)
+</style>
